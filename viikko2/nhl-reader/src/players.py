b8ae4ac0fcd90c4a38065948b9efdd9bfdf09782
Simo <ssahlman1@gmail.com>
2025-11-10 21:26:44 +0200

    2. Riippuvuuksien hyödyntäminen: Pelaajalista

---
+++ viikko2/nhl-reader/src/players.py
@@ -0,0 +1,22 @@
+import requests
+from .player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+    nationality = "FIN"
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == nationality:
+            players.append(player)
+
+    print(f"Players from {nationality}:")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
